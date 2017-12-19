6208f2111b2b56c49502491375db051bcefc531e
Keran <kerbraich@gmail.com>
2017-12-19 16:18:42 +0000

    fix spacing

--- jasmine-standalone-2.8.0/src/fizzBuzz.js
+++ jasmine-standalone-2.8.0/src/fizzBuzz.js
@@ -1,20 +1,20 @@
 function FizzBuzz() {
 };
 
-  FizzBuzz.prototype.game = function(number) {
-    if (this._isDivisibleBy(15, number)) {
-      return "FizzBuzz"
-    }
-    else if (this._isDivisibleBy(3, number)) {
-      return "Fizz"
-    }
-    else if (this._isDivisibleBy(5, number)) {
-      return "Buzz"
-    }
-    else {
-      return number
-    };
+FizzBuzz.prototype.game = function(number) {
+  if (this._isDivisibleBy(15, number)) {
+    return "FizzBuzz"
+  }
+  else if (this._isDivisibleBy(3, number)) {
+    return "Fizz"
+  }
+  else if (this._isDivisibleBy(5, number)) {
+    return "Buzz"
+  }
+  else {
+    return number
   };
+};
 
 FizzBuzz.prototype._isDivisibleBy = function(divisibleBy, numberGiven) {
   return numberGiven % divisibleBy === 0
