1876e3d6639d2a4fa6936db893668e9640122c82
Lan Pham <lanpham312@gmail.com>
2017-12-19 13:46:15 +0000

    writing first jasmine test

---
+++ jasmine-standalone-2.8.0/spec/fizzBuzzSpec.js
@@ -0,0 +1,7 @@
+describe("FizzBuzz", function() {
+  describe("print the number when given a number", function() {
+    it("prints '1' when given 1", function() {
+      expect(FizzBuzz(1)).toEqual(1)
+    });
+  });
+});
