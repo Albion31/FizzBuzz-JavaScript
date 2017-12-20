02ff8df0cb1c34b16b4aef32ebdede09bbcd29b1
Lan Pham <lanpham312@gmail.com>
2017-12-20 10:51:02 +0000

    refactoring fizzbuzz to a one liner

--- jasmine-standalone-2.8.0/src/fizzBuzz.js
+++ jasmine-standalone-2.8.0/src/fizzBuzz.js
@@ -2,18 +2,20 @@ function FizzBuzz() {
 };
 
 FizzBuzz.prototype.game = function(number) {
-  if (this._isDivisibleBy(15, number)) {
-    return "FizzBuzz"
-  }
-  else if (this._isDivisibleBy(3, number)) {
-    return "Fizz"
-  }
-  else if (this._isDivisibleBy(5, number)) {
-    return "Buzz"
-  }
-  else {
-    return number
-  };
+  return this._isDivisibleBy(15, number) ? "FizzBuzz" : this._isDivisibleBy(3, number) ?
+  "Fizz" : this._isDivisibleBy(5, number) ? "Buzz" : number
+  // if (this._isDivisibleBy(15, number)) {
+  //   return "FizzBuzz"
+  // }
+  // else if (this._isDivisibleBy(3, number)) {
+  //   return "Fizz"
+  // }
+  // else if (this._isDivisibleBy(5, number)) {
+  //   return "Buzz"
+  // }
+  // else {
+  //   return number
+  // };
 };
 
 FizzBuzz.prototype._isDivisibleBy = function(divisibleBy, numberGiven) {
